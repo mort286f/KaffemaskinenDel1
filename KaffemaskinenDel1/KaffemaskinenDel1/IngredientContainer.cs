850959400c3d8a36e78cd9fad01cb6f4a39dd63f
mortenvestergaard <57791299+mortenvestergaard@users.noreply.github.com>
2021-06-09 12:26:03 +0200

    Add files via upload

---
+++ KaffemaskinenDel1/KaffemaskinenDel1/IngredientContainer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaffemaskinenDel1
+{
+    class IngredientContainer
+    {
+        public enum ingredients { coffee, }
+
+        public string FillIngredients(ingredients ingredient)
+        {
+            if (ingredient.Equals(ingredients.coffee))
+            {
+                return "Filling inrgredient container with coffee";
+            }
+            else
+            {
+                return "Invalid ingredient inserted";
+            }
+        }
+    }
+}
